6df393ce6a3d38b24529e6a42aec14c7e7dda800
haashirk2003 <haashirk2003@gmail.com>
2022-12-06 04:01:50 -0500

    Colorblind mode added + MVC Template created + RunApp Class. Make a main menu for better user interface.

---
+++ snakeGame/src/runApp.java
@@ -0,0 +1,21 @@
+import View.SnakeView;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import snakeModel.Game;
+
+public class runApp extends Application {
+
+    Game game;
+    SnakeView view;
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.game = new Game();
+        this.view = new SnakeView(this.game, primaryStage);
+    }
+}
